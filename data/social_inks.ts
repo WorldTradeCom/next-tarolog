790886ae28744737b6e6f13ba203da58dcdf53cc
DUB1401 <vlad.milosta@yandex.ru>
2026-01-12 21:02:10 +0300

    Update link

--- data/social_inks.ts
+++ data/social_inks.ts
@@ -6,7 +6,7 @@ export const Main: Record<ButtonType, { link: string; label: string } >= {
 		label: "Telegram",
 	},
 	whatsapp: {
-		link: "https://wa.me/79290468169?text=Здравствуйте, Галина! Я бы хотела заказать у вас расклад. Скажите, пожалуйста, что для этого нужно?",
+		link: "https://wa.me/380505485357?text=Здравствуйте, Галина! Я бы хотела заказать у вас расклад. Скажите, пожалуйста, что для этого нужно?",
 		label: "WhatsApp",
 	},
 	max: {
