df2c82d006c41f98011ab4cc47e31ed354ff6557
9realist2 <9realist2@gmail.com>
2026-04-24 21:01:31 +0300

    Update social_inks.ts

--- data/social_inks.ts
+++ data/social_inks.ts
@@ -1,31 +1,31 @@
 import { ButtonType } from "@/components/SocialButton";
 
-export const Main: Record<ButtonType, { link: string; label: string } >= {
-	telegram: {
-		link: "https://t.me/m/iSV7Zhx9YzZi",
-		label: "Telegram",
-	},
-	whatsapp: {
-		link: "https://wa.me/380973705851?text=Здравствуйте, Галина! Я бы хотела заказать у вас расклад. Скажите, пожалуйста, что для этого нужно?",
-		label: "WhatsApp",
-	},
-	max: {
-		link: "https://max.ru/u/f9LHodD0cOIT7N6XceeCgmeeFpIv9GtCguy7PA9WTikniGLgjz0BR-gD8Bo",
-		label: "Max",
-	}
+export const Main: Record<ButtonType, { link: string; label: string }> = {
+  telegram: {
+    link: "/go?to=tg_main",
+    label: "Telegram",
+  },
+  whatsapp: {
+    link: "/go?to=wa",
+    label: "WhatsApp",
+  },
+  max: {
+    link: "/go?to=max",
+    label: "Max",
+  },
 };
 
-export const Secondary: Record<ButtonType, { link: string; label: string } >= {
-	telegram: {
-		link: "https://t.me/m/betRcUGkYTNi",
-		label: "Telegram",
-	},
-	whatsapp: {
-		link: "https://wa.me/380973705851?text=Здравствуйте, Галина! Я бы хотела заказать у вас расклад. Скажите, пожалуйста, что для этого нужно?",
-		label: "WhatsApp",
-	},
-	max: {
-		link: "https://max.ru/u/f9LHodD0cOIT7N6XceeCgmeeFpIv9GtCguy7PA9WTikniGLgjz0BR-gD8Bo",
-		label: "Max",
-	}
+export const Secondary: Record<ButtonType, { link: string; label: string }> = {
+  telegram: {
+    link: "/go?to=tg_sec",
+    label: "Telegram",
+  },
+  whatsapp: {
+    link: "/go?to=wa",
+    label: "WhatsApp",
+  },
+  max: {
+    link: "/go?to=max",
+    label: "Max",
+  },
 };
