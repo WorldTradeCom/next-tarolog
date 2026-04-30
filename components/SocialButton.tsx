32aeb571020578a63c432ed5a1f7ee7c9a44f8bf
9realist2 <9realist2@gmail.com>
2026-04-30 20:04:28 +0300

    Update SocialButton.tsx

--- components/SocialButton.tsx
+++ components/SocialButton.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export type ButtonType = "telegram" | "whatsapp" | "max";
 
 interface SocialButtonProps {
@@ -16,17 +14,18 @@ export default function SocialButton({ type, color, config }: SocialButtonProps)
 		<a
 			href={link}
 			target="_blank"
-			className="gap-2 px-6 py-3 rounded-md min-w-[280px] text-white transition-transform duration-300 hover:scale-105 active:brightness-75"
+			rel="noopener noreferrer"
+			className="flex gap-2 px-6 py-3 rounded-md min-w-[280px] text-white no-underline transition-transform duration-300 hover:scale-105 active:brightness-75"
 			style={{ backgroundColor: color }}
 		>
 			<div className="w-full flex items-center justify-between">
-				<span className="text-2xl">{label}</span>
-				<Image
-					src={ImageLink}
+				<span className="text-2xl text-white font-sans">{label}</span>
+				<img
 					alt={label}
 					width={24}
 					height={24}
 					className="object-contain"
+					src={ImageLink}
 				/>
 			</div>
 		</a>
