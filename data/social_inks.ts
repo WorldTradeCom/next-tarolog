596972fec3a1af0d141ca47e49199a46f9cdaf4a
9realist2 <9realist2@gmail.com>
2026-04-30 18:22:16 +0300

    Update social_inks.ts

--- data/social_inks.ts
+++ data/social_inks.ts
@@ -2,11 +2,11 @@ import { ButtonType } from "@/components/SocialButton";
 
 export const Main: Record<ButtonType, { link: string; label: string }> = {
   telegram: {
-    link: "https://t.me/m/QPpIPdEmMzA6",
+    link: "tg://resolve?domain=galina_tarot&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C%20%D0%93%D0%B0%D0%BB%D0%B8%D0%BD%D0%B0!%0A%0A%D0%AF%20%D0%B1%D1%8B%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B0%20%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C%20%D1%83%20%D0%B2%D0%B0%D1%81%20%D1%80%D0%B0%D1%81%D0%BA%D0%BB%D0%B0%D0%B4!%0A%0A%D0%A1%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%2C%20%D0%BF%D0%BE%D0%B6%D0%B0%D0%BB%D1%83%D0%B9%D1%81%D1%82%D0%B0%2C%20%D1%87%D1%82%D0%BE%20%D0%B4%D0%BB%D1%8F%20%D1%8D%D1%82%D0%BE%D0%B3%D0%BE%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%3F",
     label: "Telegram",
   },
   whatsapp: {
-    link: "https://wa.me/380973705851?text=Здравствуйте, Галина! Я бы хотела заказать у вас расклад. Скажите, пожалуйста, что для этого нужно?",
+    link: "whatsapp://send?phone=380973705851&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C%20%D0%93%D0%B0%D0%BB%D0%B8%D0%BD%D0%B0!%0A%0A%D0%AF%20%D0%B1%D1%8B%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B0%20%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C%20%D1%83%20%D0%B2%D0%B0%D1%81%20%D1%80%D0%B0%D1%81%D0%BA%D0%BB%D0%B0%D0%B4!%0A%0A%D0%A1%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%2C%20%D0%BF%D0%BE%D0%B6%D0%B0%D0%BB%D1%83%D0%B9%D1%81%D1%82%D0%B0%2C%20%D1%87%D1%82%D0%BE%20%D0%B4%D0%BB%D1%8F%20%D1%8D%D1%82%D0%BE%D0%B3%D0%BE%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%3F",
     label: "WhatsApp",
   },
   max: {
@@ -17,11 +17,11 @@ export const Main: Record<ButtonType, { link: string; label: string }> = {
 
 export const Secondary: Record<ButtonType, { link: string; label: string }> = {
   telegram: {
-    link: "https://t.me/m/QPpIPdEmMzA6",
+    link: "tg://resolve?domain=galina_tarot&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C%20%D0%93%D0%B0%D0%BB%D0%B8%D0%BD%D0%B0!%0A%0A%D0%AF%20%D0%B1%D1%8B%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B0%20%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C%20%D1%83%20%D0%B2%D0%B0%D1%81%20%D1%80%D0%B0%D1%81%D0%BA%D0%BB%D0%B0%D0%B4!%0A%0A%D0%A1%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%2C%20%D0%BF%D0%BE%D0%B6%D0%B0%D0%BB%D1%83%D0%B9%D1%81%D1%82%D0%B0%2C%20%D1%87%D1%82%D0%BE%20%D0%B4%D0%BB%D1%8F%20%D1%8D%D1%82%D0%BE%D0%B3%D0%BE%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%3F",
     label: "Telegram",
   },
   whatsapp: {
-    link: "https://wa.me/380973705851?text=Здравствуйте, Галина! Я бы хотела заказать у вас расклад. Скажите, пожалуйста, что для этого нужно?",
+    link: "whatsapp://send?phone=380973705851&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C%20%D0%93%D0%B0%D0%BB%D0%B8%D0%BD%D0%B0!%0A%0A%D0%AF%20%D0%B1%D1%8B%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B0%20%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C%20%D1%83%20%D0%B2%D0%B0%D1%81%20%D1%80%D0%B0%D1%81%D0%BA%D0%BB%D0%B0%D0%B4!%0A%0A%D0%A1%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%2C%20%D0%BF%D0%BE%D0%B6%D0%B0%D0%BB%D1%83%D0%B9%D1%81%D1%82%D0%B0%2C%20%D1%87%D1%82%D0%BE%20%D0%B4%D0%BB%D1%8F%20%D1%8D%D1%82%D0%BE%D0%B3%D0%BE%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%3F",
     label: "WhatsApp",
   },
   max: {
